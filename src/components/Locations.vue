15ff89e19bf99cb7c53f66536e924f9db7205913
JosefinE7 <fin.jos@hotmail.com>
2021-11-25 01:26:03 +0100

    Changed label of submit button to Create

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -43,7 +43,7 @@
                 <Textarea inputId="directions" v-model="value" :autoResize="true" rows="5" cols="30" required />
             </div>
         </div>
-        <Button label="Submit" type="submit" icon="pi pi-check" iconPos="left" @click="submit" />
+        <Button label="Create" type="submit" icon="pi pi-check" iconPos="left" @click="submit" />
         <Toast />
     </form>
 </template>
